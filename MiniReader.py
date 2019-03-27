9941cee5c1b06585e7d6212a5326daa1a21166e9
Ruslan Ronzhin <rus-108@yandex.ru>
2019-03-27 22:47:51 +0500

    Save and text_from_html methods added

--- MiniReader.py
+++ MiniReader.py
@@ -1,3 +1,5 @@
+import argparse
+import os
 import requests
 from bs4 import BeautifulSoup
 import textwrap
@@ -11,20 +13,47 @@ def get_html(url):
 class Article:
 
     def __init__(self, url):
-        self.soup = BeautifulSoup(get_html(url), features="html.parser")
-        self.blocks = self.soup.find_all(['p', 'h1', 'h2', 'h3'])
+        self.url = url
+        self.tags_to_read = ['p', 'h1', 'h2', 'h3']
 
-    def print(self):
-        for block in self.blocks:
-            print(textwrap.fill(block.text, 80))
-            print()
+        self.text = self.text_from_html(get_html(self.url))
+
+    def text_from_html(self, html):
+        soup = BeautifulSoup(html, features="html.parser")
+        blocks = soup.find_all(self.tags_to_read)
+        text = ''
+
+        for block in blocks:
+            text += textwrap.fill(block.text, 80) + '\n\n'
+        return text
+
+    def save(self):
+
+        if self.url.startswith('http://'):
+            cleaned_url = self.url[7:]
+        elif self.url.startswith('https://'):
+            cleaned_url = self.url[8:]
+        else:
+            cleaned_url = self.url
+
+        folders = cleaned_url.split('/')[:-1]
+        path = './'
+        for i, folder in enumerate(folders):
+            path += folder + '/'
+            if not os.path.exists(path):
+                os.mkdir(path)
+
+        filename = cleaned_url.split('/')[-1].split('.')[0] + '.txt'
+        with open(path + filename, 'w') as file:
+            file.write(self.text)
 
 
 def main():
     url = 'https://meduza.io/slides/prishlos-120-chasov-podmetat-pol-i-myt-tramvai-v-transportnom-depo'
 
     article = Article(url)
-    article.print()
+    print(article.text)
+    article.save()
 
 
 if __name__ == '__main__':
