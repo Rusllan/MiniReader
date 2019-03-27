b802d600e7bde906f82ca91d3e71b3ffa282942a
Ruslan Ronzhin <rus-108@yandex.ru>
2019-03-27 20:59:45 +0500

    Simple Article class created

--- MiniReader.py
+++ MiniReader.py
@@ -8,15 +8,23 @@ def get_html(url):
     return r.text
 
 
+class Article:
+
+    def __init__(self, url):
+        self.soup = BeautifulSoup(get_html(url), features="html.parser")
+        self.blocks = self.soup.find_all(['p', 'h1', 'h2', 'h3'])
+
+    def print(self):
+        for block in self.blocks:
+            print(textwrap.fill(block.text, 80))
+            print()
+
+
 def main():
     url = 'https://meduza.io/slides/prishlos-120-chasov-podmetat-pol-i-myt-tramvai-v-transportnom-depo'
-    soup = BeautifulSoup(get_html(url), features="html.parser")
 
-    blocks = soup.find_all(['p', 'h1', 'h2', 'h3'])
-
-    for block in blocks:
-        print(textwrap.fill(block.text, 80))
-        print()
+    article = Article(url)
+    article.print()
 
 
 if __name__ == '__main__':
